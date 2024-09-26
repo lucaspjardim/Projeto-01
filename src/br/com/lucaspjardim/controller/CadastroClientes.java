4413eba3c24039df290bd3feae20aaa90e0c7c8e
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-09-26 15:49:52 -0300

    Commit inicial, criando estrutura, pastas e arquivos

---
+++ src/br/com/lucaspjardim/controller/CadastroClientes.java
@@ -0,0 +1,7 @@
+package br.com.lucaspjardim.controller;
+
+/**
+ * Author: Lucas Jardim
+ */
+public class CadastroClientes {
+}
---
+++ src/br/com/lucaspjardim/model/Cliente.java
@@ -0,0 +1,7 @@
+package br.com.lucaspjardim.model;
+
+/**
+ * Author: Lucas Jardim
+ */
+public class Cliente {
+}
